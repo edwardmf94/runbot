f85bab6fff3d491fcaa338752c68d032a0e872d0
Christophe Monniez <moc@odoo.com>
2021-03-25 17:25:34 +0100

    WIP

--- runbot/controllers/frontend.py
+++ runbot/controllers/frontend.py
@@ -427,20 +427,21 @@ class Runbot(Controller):
 
         return {build.id : {stat.key.split('.')[1]: stat.value for stat in build.stat_ids if stat.key.startswith(key_category)} for build in builds}
 
-    @route(['/runbot/stats/<string:module>/<string:category>/<int:bundle_id>/<int:params_id>'], type='http', auth="public", website=True)
-    def module_stats(self, module, category, bundle_id, params_id, search=None, **post):
-        """Module statistics"""
+    @route(['/runbot/stats/<string:category>/<int:bundle_id>/<int:params_id>'], type='http', auth="public", website=True)
+    def modules_stats(self, category, bundle_id, params_id, search=None, **post):
+        """Modules statistics"""
 
         params = request.env['runbot.build.params'].browse(params_id)
         bundle = request.env['runbot.bundle'].browse(bundle_id)
         if not params.exists() or not bundle.exists():
             return request.not_found()
 
+        categories = request.env['runbot.build.stat.regex'].search([]).mapped('name')
+
         context = {
-            'category': category,
-            'module': module,
+            'stats_categories': categories,
             'bundle': bundle,
             'params': params,
         }
 
-        return request.render("runbot.module_stats", context)
+        return request.render("runbot.modules_stats", context)
--- runbot/static/src/js/stats.js
+++ runbot/static/src/js/stats.js
@@ -4,17 +4,18 @@ var config = {
   options: {
     legend: {
         display: true,
+        position: 'right',
     },
     responsive: true,
     tooltips: {
-      mode: 'index'
+      mode: 'point'
     },
     scales: {
       xAxes: [{
         display: true,
         scaleLabel: {
           display: true,
-          labelString: 'Batches'
+          labelString: 'Builds'
         }
       }],
       yAxes: [{
@@ -48,38 +49,90 @@ function fetch(path, data, then) {
     };
 
 function random_color(module_name){
-    var seed = Array.from(module_name).map(c => c.charCodeAt(0)).reduce((a, b) => (a << 5 - a) + b, 0);
-    var r = seed & 255;
-    var g = seed >> 8 & 255;
-    var b = seed >> 16 & 255;
-    return 'rgb(' + r + ', ' + g + ', ' + b + ')'
+    var colors = ['#004acd', '#3658c3', '#4a66ba', '#5974b2', '#6581aa', '#6f8fa3', '#7a9c9d', '#85a899', '#91b596', '#a0c096', '#fdaf56', '#f89a59', '#f1865a', '#e87359', '#dc6158', '#ce5055', '#bf4150', '#ad344b', '#992a45', '#84243d'];
+    var sum = 0;
+    for (var i = 0; i < module_name.length; i++) {
+        sum += module_name.charCodeAt(i);
+    }
+    sum = sum % colors.length;
+    color = colors[sum];
+
+    return color
 };
 
-window.onload = function() {
-  fetch("/runbot/stats/1/3/module_loading_queries", {}, function(result){
-    console.log(result);
-    var builds = Object.keys(result);
-    var first_build_stats = result[builds[0]];
-    var modules = Object.keys(first_build_stats);
 
-    modules.sort((m1, m2) => first_build_stats[m2] - first_build_stats[m1]);
+function process_chart(){
+    var builds = Object.keys(config.result);
+    var newer_build_stats = config.result[builds[0]];
+    var older_build_stats = config.result[builds.slice(-1)[0]];
+
+    var mode = document.getElementById('mode_selector').value;
+
+    function display_value(module, build_stats){
+        // {'base': 50, 'crm': 25 ...}
+        if (build_stats === undefined)
+            build_stats = newer_build_stats;
+        if (build_stats[module] === undefined)
+            return NaN;
+        if (mode == 'normal')
+            return build_stats[module]
+        if (older_build_stats[module] === undefined)
+            return NaN;
+        return build_stats[module] - older_build_stats[module]
+    }
+
+    var modules = Object.keys(newer_build_stats);
+
+    modules.sort((m1, m2) => Math.abs(display_value(m2)) - Math.abs(display_value(m1)));
 
     modules = modules.slice(0, 20);
-    console.log(modules);
 
     config.data = {
         labels: builds,
         datasets: modules.map(function (key){
             return {
                 label: key,
-                data: builds.map(build => result[build][key] || NaN),
+                data: builds.map(build => display_value(key, config.result[build])),
                 borderColor: random_color(key),
                 backgroundColor: 'rgba(0, 0, 0, 0)',
                 lineTension: 0
             }
         })
       };
+}
+
+function fetch_stats(fetch_url) {
+  fetch(fetch_url, {}, function(result){
+    config.result = result;
+    process_chart();
     var ctx = document.getElementById('canvas').getContext('2d');
-    window.myLine = new Chart(ctx, config);
+    window.statsChart = new Chart(ctx, config);
   });
+};
+
+function compute_url(){
+    var stat_category = document.getElementById('category_selector').value;
+    var bundle_id = 1;
+    var trigger_id = 3;
+    return '/runbot/stats/' + bundle_id + '/' + trigger_id + '/' + stat_category
+};
+
+window.onload = function() {
+
+    var mode_selector = document.getElementById('mode_selector');
+    var category_selector= document.getElementById('category_selector');
+    //does not work
+    config.options.scales.yAxes[0].scaleLabel = category_selector["options"][category_selector["selectedIndex"]].text;
+    fetch_stats(compute_url());
+
+    mode_selector.onchange = function() {
+        process_chart(this.value);
+        window.statsChart.update();
+    };
+
+    category_selector.onchange = function() {
+        //does not work
+        config.options.scales.yAxes[0].scaleLabel = this.value;
+        fetch_stats(compute_url());
+    }
 };
